eb7bd8433f9da23659df52b243df322616bf6111
LolinEagle <francois-lolin@live.fr>
2025-09-16 23:54:52 +0200

    Add game over panel & Balance the stats

--- Assets/Scripts/PlayerScripts/PlayerStats.cs
+++ Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -3,27 +3,30 @@ using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour{
 	[Header("Other elements references")]
-	[SerializeField] private Animator			animator;
-	[SerializeField] private MoveBehaviour		playerMovementScript;
+	[SerializeField] private Animator					animator;
+	[SerializeField] private MoveBehaviour				playerMovementScript;
+	[SerializeField] private GameObject					gameOverPanel;
+	[SerializeField] private GameObject					hud;
+	[SerializeField] private ThirdPersonOrbitCamBasic	cameraScript;
+	
+	[Header("Player Stats")]
+	[HideInInspector] public float	currentHealth;
+	[HideInInspector] public float	currentHunger;
+	[HideInInspector] public float	currentThirs;
+	[SerializeField] private float	maxHealth = 100f;
+	[SerializeField] private float	maxHunger = 100f;
+	[SerializeField] private float	maxThirs = 100f;
+	[SerializeField] private Image	BarFillHealth;
+	[SerializeField] private Image	BarFillHunger;
+	[SerializeField] private Image	BarFillThirs;
+	[SerializeField] private float	healthDecreaseRate;
+	[SerializeField] private float	hungerDecreaseRate;
+	[SerializeField] private float	thirsDecreaseRate;
 
-	public float currentHealth;
-	public float currentHunger;
-	public float currentThirs;
-
-	[SerializeField] private float maxHealth = 100f;
-	[SerializeField] private float maxHunger = 100f;
-	[SerializeField] private float maxThirs = 100f;
-
-	[SerializeField] private Image BarFillHealth;
-	[SerializeField] private Image BarFillHunger;
-	[SerializeField] private Image BarFillThirs;
-
-	[SerializeField] private float healthDecreaseRate;
-	[SerializeField] private float hungerDecreaseRate;
-	[SerializeField] private float thirsDecreaseRate;
-
+	[Header("Armor")]
 	public float	currentArmorPoints;
-	public bool		isDead = false;
+
+	[HideInInspector] public bool	isDead = false;
 
 	void Awake(){
 		currentHealth = maxHealth;
@@ -41,6 +44,11 @@ public class PlayerStats : MonoBehaviour{
 		hungerDecreaseRate = 0;
 		thirsDecreaseRate = 0;
 		animator.SetTrigger("Die");
+
+		// Show Game Over screen
+		hud.SetActive(false);
+		cameraScript.enabled = false;
+		gameOverPanel.SetActive(true);
 	}
 
 	public void		TakeDamage(float damage, bool overtime = false){
